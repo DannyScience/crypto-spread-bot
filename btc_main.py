76490d8ad78015dc4fc42613f6d3e353dff5fc1b
unknown <s.denis1125@gmail.com>
2022-07-16 01:43:03 +0300

    fixing code

--- btc_main.py
+++ btc_main.py
@@ -1,26 +1,38 @@
 import requests
 import json
 
-# получаем цену биткоина на бинанс фьючерсах
+# obtaining BTC price from Binance
 url = 'https://fapi.binance.com/fapi/v1/ticker/price'
 param = {'symbol': 'BTCUSDT'}
 r = requests.get(url, params=param)
 if r.status_code == 200:
-    # data = json.dumps(r.json()) - дает ошибку при обращении к словарю
+    # data = json.dumps(r.json()) - error
     data = r.json()
-    print(data)
 else:
     print('error')
-a = data['price']
-print(a)
+bpbtc = float(data['price'])
+print(bpbtc)  # bpbtc = b -binance + p-price + btc - ticker
 
-# получаем цену биткоина на huobi
-url2 = 'https://api.huobi.pro/v1/common/symbols'
+# obtaining BTC price from Huobi
+url2 = 'https://api.huobi.pro/market/detail/merged?symbol=btcusdt'
+param = {'symbol': 'BTCUSDT'}
 r2 = requests.get(url2, params=param)
 if r2.status_code == 200:
     data2 = r2.json()
-    print(data2)
+    # print(data2)
 else:
     print('error2')
-a2 = data2['price']
-print(a2)
+hpbtc0 = data2['tick']
+hpbtc00 = hpbtc0['bid']
+hpbtc = float(hpbtc00[0])
+print(hpbtc)
+
+# compairing prices and counting a percent difference
+if float(hpbtc) > float(bpbtc):
+    hbpd = hpbtc - bpbtc
+    hbpdpercent = hbpd / hpbtc * 100
+    print(hbpdpercent)
+else:
+    hbpd = bpbtc - hpbtc
+    hbpdpercent = hbpd / hpbtc * 100
+    print(hbpdpercent)
